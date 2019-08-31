f7dfde107818447636ca9de8f9cded46fedfa63b
imjwpak83 <imjwpak83@naver.com>
2019-08-31 15:24:05 +0900

    20190831 15:24

--- calculator/controller.py
+++ calculator/controller.py
@@ -16,18 +16,18 @@ class CalculatorController:
         tf.reset_default_graph()
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
-            saver = tf.train.import_meta_graph('calculator/saved_add_model/model-1000.meta')
+            saver = tf.train.import_meta_graph('calculator/saved_' + ocode + '_model/model-1000.meta')
             # checkpoint : 훈련에 대한 로그를 찍어내는 것
             # 가장 똑똑한게 마지막 checkpoint일거니까
             # 훈련한 기록을 로그로 남김
-            saver.restore(sess, tf.train.latest_checkpoint('calculator/saved_add_model'))
+            saver.restore(sess, tf.train.latest_checkpoint('calculator/saved_' + ocode + '_model'))
             # 좌표로 식을 찍음
             graph = tf.get_default_graph()
             w1 = graph.get_tensor_by_name('w1:0')
             w2 = graph.get_tensor_by_name('w2:0')
             # tensorflow는 내부적으로 다 실수 처리한다
             feed_dict = {w1: float(n1), w2: float(n2)}
-            op_to_restore = graph.get_tensor_by_name('op_add:0')
+            op_to_restore = graph.get_tensor_by_name('op_' + ocode + ':0')
             result = sess.run(op_to_restore, feed_dict)
             print('텐서가 계산한 결과: {}'.format(result))
 
--- calculator/model.py
+++ calculator/model.py
@@ -23,3 +23,51 @@ class CalculatorModel:
         print('TF 덧셈결과: {}'.format(result))
         # 1000번을 실행해서 모델이 이게 덧셈이라는 것을 이해하도록 함
         saver.save(sess, './saved_add_model/model', global_step=1000)
+
+    def create_sub_model(self):
+        # tf.placeholder 변수 처리 하는 곳. 실수가 기본
+        w1 = tf.placeholder(tf.float32, name='w1')
+        w2 = tf.placeholder(tf.float32, name='w2')
+        # 값을 공급
+        feed_dict = {'w1': 8.0, 'w2': 2.0}  # dummy값으로 초기화 하는 작업으로 보면 됨
+        # op_sub라는 이름의 모델을 만든 것
+        r = tf.subtract(w1, w2, name='op_sub')
+        sess = tf.Session()
+        _ = tf.Variable(initial_value='fake_variable')  # _는 임시 변수라고 보면 됨
+        sess.run(tf.global_variables_initializer())
+        saver = tf.train.Saver()  # 저장 작업을 먼저 해야 저장된 것으로 학습을 하니 우선적으로 수행
+        result = sess.run(r, {w1: feed_dict['w1'], w2: feed_dict['w2']})
+        print('TF 뺄셈결과: {}'.format(result))
+        saver.save(sess, './saved_sub_model/model', global_step=1000)
+
+    def create_mul_model(self):
+        # tf.placeholder 변수 처리 하는 곳. 실수가 기본
+        w1 = tf.placeholder(tf.float32, name='w1')
+        w2 = tf.placeholder(tf.float32, name='w2')
+        # 값을 공급
+        feed_dict = {'w1': 8.0, 'w2': 2.0}  # dummy값으로 초기화 하는 작업으로 보면 됨
+        # op_sub라는 이름의 모델을 만든 것
+        r = tf.multiply(w1, w2, name='op_mul')
+        sess = tf.Session()
+        _ = tf.Variable(initial_value='fake_variable')  # _는 임시 변수라고 보면 됨
+        sess.run(tf.global_variables_initializer())
+        saver = tf.train.Saver()  # 저장 작업을 먼저 해야 저장된 것으로 학습을 하니 우선적으로 수행
+        result = sess.run(r, {w1: feed_dict['w1'], w2: feed_dict['w2']})
+        print('TF 곱셈결과: {}'.format(result))
+        saver.save(sess, './saved_mul_model/model', global_step=1000)
+
+    def create_div_model(self):
+        # tf.placeholder 변수 처리 하는 곳. 실수가 기본
+        w1 = tf.placeholder(tf.float32, name='w1')
+        w2 = tf.placeholder(tf.float32, name='w2')
+        # 값을 공급
+        feed_dict = {'w1': 8.0, 'w2': 2.0}  # dummy값으로 초기화 하는 작업으로 보면 됨
+        # op_sub라는 이름의 모델을 만든 것
+        r = tf.divide(w1, w2, name='op_div')
+        sess = tf.Session()
+        _ = tf.Variable(initial_value='fake_variable')  # _는 임시 변수라고 보면 됨
+        sess.run(tf.global_variables_initializer())
+        saver = tf.train.Saver()  # 저장 작업을 먼저 해야 저장된 것으로 학습을 하니 우선적으로 수행
+        result = sess.run(r, {w1: feed_dict['w1'], w2: feed_dict['w2']})
+        print('TF 나눗셈결과: {}'.format(result))
+        saver.save(sess, './saved_div_model/model', global_step=1000)
